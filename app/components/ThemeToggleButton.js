674398337ce34deaf58a6ffcd4b72996a8b646d3
Suraj Gaur <surajgaur242@gmail.com>
2023-12-01 14:18:55 +0530

    add some specific code

--- app/components/ThemeToggleButton.js
+++ app/components/ThemeToggleButton.js
@@ -1,14 +1,18 @@
 'use client'
 import React from 'react';
+import { ThemeContext } from './ThemeProvider';
 
 
 const ThemeToggleButton = () => {
+
+    const { theme, toggleTheme } = React.useContext(ThemeContext);
     
     return (
        <>
-       
+           <button onClick={toggleTheme}>
+            Toggle local theme to {theme === "light" ? "dark" : "light" }</button>
        </>
     )
 
-}
+};
 export {ThemeToggleButton}
